fbb0f9a9e8c10f220496481815ee2a91616b906a
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-09-17 20:18:44 -0700

    Fix merge conflict markers, restore cart badge, fix static product detail page, reorganize assets into public folder

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,38 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false,
+) {
+  const htmlStrings = list.map(templateFn);
+
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function updateCartCount() {
+  const cartItems = getLocalStorage("so-cart") || [];
+  const cartCount = document.querySelector(".cart-count");
+
+  if (cartItems.length === 0) {
+    cartCount.hidden = true;
+    return;
+  }
+
+  cartCount.textContent = cartItems.length;
+  cartCount.hidden = false;
+}
