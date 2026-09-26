51fda5fbe51b4b88b4c206467c69c376556609e8
Desertelk <cadenwatts111@gmail.com>
2026-09-25 21:57:51 -0700

    Checkout now posts to server and gets a 200 response

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,10 +1,10 @@
-// import { updateCartCount } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 
 const checkout = new CheckoutProcess("so-cart", ".order-summary");
+const services = new ExternalServices();
 
 checkout.init();
-// updateCartCount();
 
 const zip = document.querySelector("#zip");
 
@@ -14,28 +14,10 @@ zip.addEventListener("blur", () => {
   }
 });
 
-// const cartItems = getLocalStorage("so-cart") || [];
-// const summaryList = document.querySelector("#order-summary-list");
-// const totalElement = document.querySelector("#order-total");
+const form = document.querySelector("#checkout-form");
 
-// function renderOrderSummary() {
-//     if (cartItems.length === 0) {
-//         summaryList.innerHTML = "<li>Your cart is empty.</li>";
-//         totalElement.textContent = "0.00";
-//         return;
-//     }
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-//     summaryList.innerHTML = cartItems.map((item) =>
-//         `<li>${item.Name} - $${Number(item.FinalPrice).toFixed(2)}</li>`
-//     ).join("");
-
-//     const total = cartItems.reduce((sum, item) =>
-//         sum + Number(item.FinalPrice),
-//         0,
-//     );
-
-//     totalElement.textContent = total.toFixed(2);
-// }
-
-// renderOrderSummary();
-// updateCartCount();
+  await checkout.checkout(form, services);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { updateCartCount } from "./utils.mjs";
 
 loadHeaderFooter();
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 const listElement = document.querySelector(".product-list");
 
 const productList = new ProductList("tents", dataSource, listElement);
